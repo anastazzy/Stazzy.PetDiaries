ac179f094a6ab8a7fd951af11dd4169cecca8e75
anastazzy <shitovanap@gmail.com>
2025-04-17 17:42:04 +0300

    feat: added swagger and base project struct

---
+++ MyPets.DataAccess/MyPetsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPets.Domain.Models;
+
+namespace MyPets.DataAccess;
+
+public class MyPetsDbContext(DbContextOptions<MyPetsDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+}
