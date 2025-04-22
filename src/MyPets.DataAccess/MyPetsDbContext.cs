4365713536d6637a6beecf7a47ca7d5fa0943d8d
anastazzy <shitovanap@gmail.com>
2025-04-22 15:40:50 +0300

    feat: add first migration

--- src/MyPets.DataAccess/MyPetsDbContext.cs
+++ src/MyPets.DataAccess/MyPetsDbContext.cs
@@ -3,8 +3,12 @@ using MyPets.Domain.Models;
 
 namespace MyPets.DataAccess;
 
-public class MyPetsDbContext(DbContextOptions<MyPetsDbContext> options) : DbContext(options)
+public sealed class MyPetsDbContext : DbContext
 {
+    public MyPetsDbContext(DbContextOptions<MyPetsDbContext> options) : base(options)
+    {
+    }
+
     public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
--- src/MyPets.Domain/Models/User.cs
+++ src/MyPets.Domain/Models/User.cs
@@ -4,6 +4,7 @@ namespace MyPets.Domain.Models;
 
 public class User
 {
+    public long Id { get; set; }
     public required string Name { get; set; }
     public required string Email { get; set; }
     public required string PasswordHash { get; set; }
